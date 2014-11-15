3f58f75b85a5d554af98f6e19583a9276145b9de
Andrey Chizh <info.andreychizh@gmail.com>
2014-11-15 19:26:14 +0200

    Add new tests for padDigits()

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 
 'use strict';
 
-exports.getDigits = function(value, digits) {
-
-
-
-    return '' + value;
+/**
+ * Get string with digits number
+ * @param value
+ * @param digits
+ * @returns {string}
+ */
+exports.padDigits = function(value, digits) {
+    var str = '' + value;
+    while (str.length < digits) { str = '0' + str; }
+    return str;
 };
--- tests/spec.js
+++ tests/spec.js
@@ -6,12 +6,25 @@ var should = require('should'),
 
 describe('Spanner', function() {
 
-    describe('#getDigits', function() {
+    describe('#getDigits()', function() {
 
-        it('should return 0 when the value is not present', function() {
-            spanner.getDigits(0).should.equal('0');
+        it('should return `0` when the value = 0 and set digit = 0', function() {
+            spanner.padDigits(0, 0).should.equal('0');
+        });
+
+        it('should return `0` when the value = 0 and set digit = 1', function() {
+            spanner.padDigits(0, 1).should.equal('0');
+        });
+
+        it('should return `00` when the value = 0 and set digit = 2', function() {
+            spanner.padDigits(0, 2).should.equal('00');
+        });
+
+        it('should return `000` when the value = 0 and set digit = 3', function() {
+            spanner.padDigits(0, 3).should.equal('000');
         });
 
     });
 
 });
+
